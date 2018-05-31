ea0c3c1de950fdf584ed64ff9e334d628c59db94
vendelal <vendelalinnea@mac.com>
2018-05-31 17:36:15 -0400

    editing images and such

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,26 @@
 // This is where it all goes :)
+
+
+// function shrinkHeader() {
+//   window.addEventListener("scroll", function(e) {
+//     var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+//         blogTitle = document.getElementsByClassName("blog-title")[0],
+//         blogTitleFirstLine = document.getElementsByClassName(
+//       "blog-title-first-line"
+//     )[0];
+//     if (distanceY < 57) {
+//       var titleSizeOffset = 87 - distanceY;
+//       blogTitle.setAttribute(
+//         "style",
+//         "font-size:" +
+//         titleSizeOffset +
+//         "px; line-height:" +
+//         titleSizeOffset +
+//         "px;"
+//       );
+//     } else {
+//       blogTitle.setAttribute("style", "font-size: 30px; line-height: 27px;");
+//     }
+//   });
+// }
+// window.onload = shrinkHeader();
